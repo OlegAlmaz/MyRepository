ed307fa558181b7240396cf834496e6ac36bde55
OlegAlmaz <almaz_2792@mail.ru>
2018-10-08 23:03:41 +0300

    уще удалены 4 символа

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello worl");
+            Console.WriteLine("hello");
 
             Console.ReadKey();
         }
